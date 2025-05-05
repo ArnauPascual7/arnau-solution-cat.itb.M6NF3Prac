3a1adcc63c9e2c2cb17c60dab7cbec40816fe7c2
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-05 10:44:30 +0200

    Created Prac Test Project

---
+++ cat.itb.storetest_PascualArnau/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cat.itb.storetest_PascualArnau
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
